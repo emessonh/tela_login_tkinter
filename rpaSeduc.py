fb34319705439aad2694dde27126bdaf82922ed4
emessonh <ehs4@aluno.ifal.edu.br>
2024-01-14 21:52:35 -0300

    funcionando selecao de documentos

--- rpaSeduc.py
+++ rpaSeduc.py
@@ -7,67 +7,104 @@ from dotenv import load_dotenv
 import os
 # import time
 
-PROXY = f'http://{os.getenv('usuario')}:{os.getenv('senha')}@{os.getenv('firewall')}'
-webdriver.DesiredCapabilities.CHROME['proxy'] = {
-"httpProxy": PROXY,
-"ftpProxy": PROXY,
-"sslProxy": PROXY,
-"proxyType": "manual",
+# PROXY = f'http://{os.getenv('usuario')}:{os.getenv('senha')}@{os.getenv('firewall')}'
+# webdriver.DesiredCapabilities.CHROME['proxy'] = {
+# "httpProxy": PROXY,
+# "ftpProxy": PROXY,
+# "sslProxy": PROXY,
+# "proxyType": "manual",
 
-}
+# }
 
-janela = Tk()
-janela.geometry('400x350')
-janela.title('Assinaturas SEI')
-janela.resizable(width=False, height=False)
-# titulo = Label(janela, text='Login SEI', pady=10).grid(row=1, columnspan=2, column=10)
-titulo = Label(janela, text='Login SEI', pady=10).grid(row=1)
-# Usuario
-label_usuario = Label(janela, text='Usuário', pady=10).grid(row=2, column=0)
-entry_usuario = Entry(janela).grid(row=2, column=1)
-# Senha
-label_senha = Label(janela, text='Senha', pady=10).grid(row=5, column=0)
-entry_senha = Entry(janela, show='*').grid(row=5, column=1)
+# Tela de login
 
-# botao.pack()
+def tela_login():
+    janela = Tk()
+    janela.geometry('400x250')
 
-# select
+    # logo seduc
+    # diretorio_atual = os.getcwd()
+    # janela.iconbitmap(fr'{diretorio_atual}\logo_seduc.ico')
+
+    janela.title('Assinaturas SEI')
+    janela.resizable(width=False, height=False)
+    # Titulo da tela
+    container_titulo = Frame(janela, padx=140, pady=10) 
+    container_titulo.grid()
+    titulo = Label(container_titulo, text='Login SEI')
+    titulo.grid()
+    # Usuario
+    container_usuario = Frame(janela, padx=140, pady=10)
+    container_usuario.grid()
+    label_usuario = Label(container_usuario, text='Usuário').grid()
+    entry_usuario = Entry(container_usuario).grid()
+    # Senha
+    container_senha = Frame(janela, padx=140, pady=10)
+    container_senha.grid()
+    label_senha = Label(container_senha, text='Senha').grid()
+    entry_senha = Entry(container_senha, show='*').grid()
+
+    # Botão confirmação
+    container_botao = Frame(janela, padx=140, pady=10)
+    container_botao.grid()
+    botao = Button(container_botao, text='Login', command=janela.destroy, width='10').grid()
+    janela.mainloop()
+
+
+
+docs_selecionados = [] 
+
+
+# Selecionar os documentos
+
+janela_docs = Tk()
+janela_docs.geometry('450x250')
+janela_docs.title('Documentos para assinatura')
+container_documentos = Frame(janela_docs, padx=100, pady=30)
+# scroll_list = Scrollbar(container_documentos)
+container_documentos.grid()
 documentos = ['Todos', 'doc 1', 'doc 2', 'doc 3', 'doc 4', 'doc 5', 'doc 6', 'doc 7', 'doc 8', 'doc 9', 'doc 10']
-sel_docs = Label(janela, text='Documentos').grid(row=9, column=0)
+sel_docs = Label(container_documentos, text='Selecione os documentos').grid()
 # combobox = ttk.Combobox(janela, state="readonly", values=documentos, height=5)
-combobox = ttk.Combobox(janela, state="disabled")
+# combobox = ttk.Combobox(container_documentos, state="disabled")
 # combobox.current(0)
-listbox = Listbox(janela, selectmode="multiple", exportselection=0, height=5, width=40)
+listbox = Listbox(container_documentos, selectmode="multiple", exportselection=0, height=5, width=40)
 for doc in documentos:
    listbox.insert(END, doc)
 
-# combobox['values'] = ('Todos', 'doc 1', 'doc 2', 'doc 3', 'doc 4', 'doc 5', 'doc 6', 'doc 7', 'doc 8', 'doc 9', 'doc 10')
 
-
-# print(listbox.size())
-# elements = print(listbox.get(FIRST, last=None))
-# for idx in elements:
-#    print(idx)
-
-# combobox.current(0)
 
 # define a function to update the combobox when the user selects or deselects a value
-def update_combobox():
-   # Get selected values from the Listbox widget
-   selected_values = [listbox.get(idx) for idx in listbox.curselection()]
+# def update_combobox():
+#    # Get selected values from the Listbox widget
+#    selected_values = [listbox.get(idx) for idx in listbox.curselection()]
     
-   # Update the combobox with the selected values
-   combobox.configure(width=40, height=5)
-   combobox.set(", ".join(selected_values))
+#    # Update the combobox with the selected values
+#    combobox.configure(width=40, height=5)
+#    combobox.set(", ".join(selected_values))
     
 # bind the update_combobox function to the Listbox widget
-listbox.bind("<<ListboxSelect>>", lambda _: update_combobox())
-combobox.grid(row=9, column=1)
-listbox.grid(row=10, column=1)
+# listbox.bind("<<ListboxSelect>>", lambda _: update_combobox())
+# combobox.grid()
+listbox.grid()
 
-# Botão confirmação
-botao = Button(janela, text='Login', command=janela.destroy, width='10').grid(row=12, columnspan=4)
-janela.mainloop()
+def seleciona_docs():
+    global janela_docs
+    global listbox
+    for i in listbox.curselection():
+        docs_selecionados.append(listbox.get(i))
+    janela_docs.destroy()
+    tela_login()
+
+container_botao = Frame(janela_docs, padx=100, pady=5)
+container_botao.grid()
+confirmacao = Button(container_botao, text='Selecionar', command=seleciona_docs)
+confirmacao.grid()
+# confirmacao.bind('<Return>', seleciona_docs(janela_docs))
+janela_docs.mainloop()
+print(docs_selecionados)
+
+# tela_login()
 
 # with webdriver.Chrome() as driver:
 #     driver.get("https://selenium.dev")
